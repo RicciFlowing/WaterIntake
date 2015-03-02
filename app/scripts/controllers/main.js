c6d0ebceece00ce6886b9ea622ad4428fba3476b
benjamin <bkirwa@mathe-sellin.de>
2015-03-02 17:44:45 +0100

    Start. Status after running yeoman angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name waterintakeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the waterintakeApp
+ */
+angular.module('waterintakeApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
